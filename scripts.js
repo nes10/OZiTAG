5b0e7214acb77fa3d4f6460ff94020e902d49fc0
nes10 <60338368+nes10@users.noreply.github.com>
2020-02-24 21:20:30 +0300

    First

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function brightnessOn() {
+    newmain = document.getElementById('main');
+    newmain.style.cssText = 'background: rgb(0, 0, 0, .8); transition: .5s'
+}
+
+function brightnessOff() {
+    newmain = document.getElementById('main');
+    newmain.style.cssText = 'background: rgb(0, 0, 0, 0); transition: .5s'
+}
+
+
