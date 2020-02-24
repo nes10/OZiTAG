42ba62cbb96becdb15d058cc35fbffad059489f2
nes10 <60338368+nes10@users.noreply.github.com>
2020-02-24 21:28:15 +0300

    First1

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-function brightnessOn() {
-    newmain = document.getElementById('main');
-    newmain.style.cssText = 'background: rgb(0, 0, 0, .8); transition: .5s'
-}
-
 function brightnessOff() {
-    newmain = document.getElementById('main');
-    newmain.style.cssText = 'background: rgb(0, 0, 0, 0); transition: .5s'
+    newMain = document.getElementById('main');
+    newMain.style.cssText = 'background-color: rgb(0, 0, 0, .8); transition: .5s'
 }
 
+function brightnessOn() {
+    newMain = document.getElementById('main');
+    newMain.style.cssText = 'background-color: rgb(0, 0, 0, .0); transition: .5s'
+}
 
+// function arrow() {
+//     newburgerSubmenu = document.getElementById('burgerSubmenu');
+//     newburgerSubmenu.style.cssText = 'display:block';
+// }
